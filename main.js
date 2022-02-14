65de1c6a082b1a358df147f2b0647395c7a00125
Alice Smith <alicesmith@Alices-MacBook-Pro.local>
2022-02-14 21:23:58 +0000

    arrays of data

---
+++ main.js
@@ -0,0 +1,32 @@
+// the 3 messages we will output will be: 1. A daily productivity task, 2. Self care activity 3. Creative inspiration
+
+const productivityTasks = [
+  "Make your bed!",
+  "Empty your dishwasher!",
+  "Take out the bins",
+  "Water the plants",
+  "Fold your laundry",
+  "Do the laundry",
+  "Hoover",
+  "Dust one room",
+];
+const selfCareActivity = [
+  "Brush your teeth",
+  "Foot scrub",
+  "Have a bubble bath",
+  "Facial",
+  "Go for a walk",
+  "Meditate",
+  "Complete a 10 minute yoga workout",
+  "Put phone on do not disturb",
+  "Pick up a craft",
+];
+const creativeInspiration = [
+  "https://wetransfer.com/thisworks/",
+  "http://www.mymodernmet.com/",
+  "http://www.itsnicethat.com/",
+  "http://www.designboom.com/",
+  "http://www.booooooom.com/",
+  "http://globalstreetart.com/",
+  "http://www.mr-cup.com/blog.html",
+];
