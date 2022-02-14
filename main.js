2e9d076e8e4d58c06080969d6434d247ff710ca1
Alice Smith <alicesmith@Alices-MacBook-Pro.local>
2022-02-14 21:45:03 +0000

    creating messages

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
 // the 3 messages we will output will be: 1. A daily productivity task, 2. Self care activity 3. Creative inspiration
 
 const productivityTasks = [
-  "Make your bed!",
-  "Empty your dishwasher!",
-  "Take out the bins",
-  "Water the plants",
-  "Fold your laundry",
-  "Do the laundry",
-  "Hoover",
-  "Dust one room",
+  "making your bed!",
+  "emptying your dishwasher!",
+  "taking out the bins!",
+  "watering the plants!",
+  "folding your laundry!",
+  "doing the laundry!",
+  "hoovering up!",
+  "doing the dusting!",
 ];
 const selfCareActivity = [
-  "Brush your teeth",
-  "Foot scrub",
-  "Have a bubble bath",
-  "Facial",
-  "Go for a walk",
-  "Meditate",
-  "Complete a 10 minute yoga workout",
-  "Put phone on do not disturb",
-  "Pick up a craft",
+  "brushing your teeth",
+  "giving yourself a foot scrub",
+  "having a bubble bath",
+  "giving yourself a facial",
+  "going for a walk",
+  "meditating",
+  "completing a 10 minute yoga workout",
+  "putting your phone on 'do not disturb'",
+  "picking up a craft",
 ];
 const creativeInspiration = [
   "https://wetransfer.com/thisworks/",
@@ -30,3 +30,20 @@ const creativeInspiration = [
   "http://globalstreetart.com/",
   "http://www.mr-cup.com/blog.html",
 ];
+
+const randomTask = (array) => {
+  const index = Math.floor(Math.random() * array.length);
+  return array[index];
+};
+
+console.log(
+  `Start the day off productively by ${randomTask(productivityTasks)}`
+);
+console.log(
+  `Make sure to look after yourself today by ${randomTask(selfCareActivity)}`
+);
+console.log(
+  `Feel creatively inspired today through taking a peek at ${randomTask(
+    creativeInspiration
+  )}`
+);
